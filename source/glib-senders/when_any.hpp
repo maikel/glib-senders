4d9302b6db82cf26ff7f1ed295c076ee30a42514
Nadolski, Maikel <maikel.nadolski@gmail.com>
2023-01-30 03:50:38 +0100

    Implement to wait for all completions

--- source/glib-senders/when_any.hpp
+++ source/glib-senders/when_any.hpp
@@ -6,84 +6,134 @@
 #include <stdexec/execution.hpp>
 
 namespace gsenders {
-
-template <typename BaseEnv>
-using when_any_env_t = stdexec::__make_env_t<
+namespace when_any_ {
+template <class BaseEnv>
+using env_t = stdexec::__make_env_t<
     BaseEnv,
     stdexec::__with<stdexec::get_stop_token_t, stdexec::in_place_stop_token>>;
 
-template <class Sender, class Receiver, class Op> struct when_any_receiver {
+template <class Sender, class Receiver, class Op> class receiver {
+public:
+  explicit receiver(Op* op) noexcept : op_{op} {}
+
+private:
   Op* op_;
 
-  template <class... Args>
-  friend void tag_invoke(stdexec::set_value_t, when_any_receiver&& self,
-                         Args&&... args) noexcept {
-    if (!self.op_->stopped_.exchange(true)) {
-      stdexec::set_value((Receiver &&) self.op_->receiver_,
-                         ((Args &&) args)...);
-    }
+  template <class CPO, class... Args>
+  void notify(CPO, Args&&... args) noexcept {
+    op_->notify(CPO{}, ((Args &&) args)...);
   }
 
-  template <class Error>
-  friend void tag_invoke(stdexec::set_error_t, when_any_receiver&& self,
-                         Error&& error) noexcept {
-    if (!self.op_->stopped_.exchange(true)) {
-      stdexec::set_error((Receiver &&) self.op_->receiver_, (Error &&) error);
-    }
+  template <stdexec::__one_of<stdexec::set_value_t, stdexec::set_error_t,
+                              stdexec::set_stopped_t>
+                CPO,
+            class... Args>
+  friend void tag_invoke(CPO, receiver&& self, Args&&... args) noexcept {
+    self.notify(CPO{}, ((Args &&) args)...);
   }
 
-  friend void tag_invoke(stdexec::set_stopped_t,
-                         when_any_receiver&& self) noexcept {
-    if (!self.op_->stopped_.exchange(true)) {
-      stdexec::set_stopped((Receiver &&) self.op_->receiver_);
-    }
-  }
-
-  friend auto tag_invoke(stdexec::get_env_t, const when_any_receiver&) noexcept
+  friend auto tag_invoke(stdexec::get_env_t, const receiver&) noexcept
       -> stdexec::__empty_env {
     return {};
   }
 };
 
-template <class Receiver, class... Senders> class when_any_operation {
+template <class... Senders> struct completion_signatures;
+
+template <class Sender, class... Senders>
+struct completion_signatures<Sender, Senders...> {
+  using AddSigl = stdexec::__minvoke<
+      stdexec::__mconcat<stdexec::__q<stdexec::completion_signatures>>,
+      stdexec::completion_signatures_of_t<Senders>...>;
+
+  using type =
+      stdexec::make_completion_signatures<Sender, stdexec::no_env, AddSigl>;
+};
+
+template <class... Senders>
+using completion_signatures_t =
+    typename completion_signatures<Senders...>::type;
+
+template <class Sig> struct signature_to_tuple;
+
+template <class Ret, class... Args> struct signature_to_tuple<Ret(Args...)> {
+  using type = std::tuple<Ret, Args...>;
+};
+
+template <class Sig>
+using signature_to_tuple_t = typename signature_to_tuple<Sig>::type;
+
+template <class... Senders>
+using result_type_t =
+    stdexec::__mapply<stdexec::__transform<stdexec::__q<signature_to_tuple_t>,
+                                           stdexec::__q<std::variant>>,
+                      completion_signatures_t<Senders...>>;
+
+template <class Receiver, class... Senders> class operation {
 public:
+  operation(operation&&) = delete;
+
   template <class SenderTuple>
-  when_any_operation(SenderTuple&& tuple, Receiver&& receiver)
-      : when_any_operation((SenderTuple &&) tuple, (Receiver &&) receiver,
-                           std::make_index_sequence<sizeof...(Senders)>()) {}
+  operation(SenderTuple&& tuple, Receiver&& receiver)
+      : operation((SenderTuple &&) tuple, (Receiver &&) receiver,
+                  std::make_index_sequence<sizeof...(Senders)>()) {}
+
+private:
+  template <class, class, class> friend class receiver;
 
   template <class SendersTuple, std::size_t... Is>
-  when_any_operation(SendersTuple&& senders, Receiver&& receiver,
-                     std::index_sequence<Is...>)
-      : receiver_{(Receiver &&) receiver},
+  operation(SendersTuple&& senders, Receiver&& rcvr, std::index_sequence<Is...>)
+      : receiver_{(Receiver &&) rcvr},
         operation_states_{stdexec::__conv{[&senders, this]() {
           return stdexec::connect(
               std::get<Is>((SendersTuple &&) senders),
-              when_any_receiver<std::tuple_element_t<Is, SendersTuple>,
-                                Receiver, when_any_operation>{this});
+              receiver<std::tuple_element_t<Is, SendersTuple>, Receiver,
+                       operation>{this});
         }}...} {}
 
-public:
   std::atomic<bool> stopped_{false};
+  std::atomic<int> count_{sizeof...(Senders)};
   Receiver receiver_;
 
+  result_type_t<Senders...> result_;
+
+  template <class CPO, class... Args>
+  void notify(CPO, Args&&... args) noexcept {
+    if (!stopped_.exchange(true)) {
+      result_.template emplace<std::tuple<CPO, std::decay_t<Args>...>>(
+          CPO{}, ((Args &&) args)...);
+    }
+    if (count_.fetch_sub(1) == 1) {
+      std::visit(
+          [this]<class Tuple>(Tuple&& result) {
+            std::apply(
+                [this]<class C, class... As>(C, As&&... args) noexcept {
+                  try {
+                    stdexec::tag_invoke(C{}, (Receiver &&) receiver_,
+                                        (As &&) args...);
+                  } catch (...) {
+                    stdexec::set_error((Receiver &&) receiver_,
+                                       std::current_exception());
+                  }
+                },
+                (Tuple &&) result);
+          },
+          (result_type_t<Senders...> &&) result_);
+    }
+  }
+
   std::tuple<stdexec::connect_result_t<
-      Senders, when_any_receiver<Senders, Receiver, when_any_operation>>...>
+      Senders, receiver<Senders, Receiver, operation>>...>
       operation_states_;
 
-  friend void tag_invoke(stdexec::start_t, when_any_operation& self) noexcept {
+  friend void tag_invoke(stdexec::start_t, operation& self) noexcept {
     std::apply([](auto&... ops) { (stdexec::start(ops), ...); },
                self.operation_states_);
   }
 };
 
-template <class Sender, class... Senders> struct when_any_sender {
-  using AddSigl = stdexec::__minvoke<
-      stdexec::__mconcat<stdexec::__q<stdexec::completion_signatures>>,
-      stdexec::completion_signatures_of_t<Senders>...>;
-
-  using completion_signatures =
-      stdexec::make_completion_signatures<Sender, stdexec::no_env, AddSigl>;
+template <class Sender, class... Senders> struct sender {
+  using completion_signatures = completion_signatures_t<Sender, Senders...>;
 
   static_assert(stdexec::__valid_completion_signatures<completion_signatures,
                                                        stdexec::no_env>);
@@ -94,12 +144,11 @@ template <class Sender, class... Senders> struct when_any_sender {
   friend auto tag_invoke(stdexec::connect_t, Self&& self,
                          R&& receiver) noexcept {
     using Receiver = std::remove_cvref_t<R>;
-    return when_any_operation<Receiver, Sender, Senders...>(
+    return operation<Receiver, Sender, Senders...>(
         (std::tuple<Sender, Senders...> &&) self.senders_, (R &&) receiver);
   }
 
-  friend auto tag_invoke(stdexec::get_attrs_t,
-                         const when_any_sender& self) noexcept
+  friend auto tag_invoke(stdexec::get_attrs_t, const sender& self) noexcept
       -> stdexec::__empty_attrs {
     return {};
   }
@@ -111,14 +160,15 @@ struct when_any_t {
   auto operator()(Senders&&... senders) const noexcept(
       std::conjunction_v<
           std::is_nothrow_constructible<std::decay_t<Senders>, Senders>...>) {
-    static_assert(stdexec::sender<when_any_sender<Senders...>>);
-    return when_any_sender<std::decay_t<Senders>...>{
+    static_assert(stdexec::sender<sender<Senders...>>);
+    return sender<std::decay_t<Senders>...>{
         std::tuple<std::decay_t<Senders>...>{((Senders &&) senders)...}};
   }
 };
 
 inline constexpr when_any_t when_any{};
-
+} // namespace when_any_
+using when_any_::when_any;
 } // namespace gsenders
 
 #endif
