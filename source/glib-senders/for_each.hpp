9f0b8d843ca48972a0bf1948934d02a6f6cd0802
Nadolski, Maikel <maikel.nadolski@gmail.com>
2023-01-30 13:23:16 +0100

    Further improvments to for_each, not yet.

--- source/glib-senders/for_each.hpp
+++ source/glib-senders/for_each.hpp
@@ -1,40 +1,116 @@
 #ifndef GLIB_SENDERS_FOR_EACH_HPP
 #define GLIB_SENDERS_FOR_EACH_HPP
 
+#include <stdexec/execution.hpp>
+
 namespace gsenders {
 namespace for_each_ {
+using env_t = stdexec::__make_env_t<
+    stdexec::__empty_env,
+    stdexec::__with<stdexec::get_stop_token_t, stdexec::in_place_stop_token>>;
+
 template <class... Senders> struct operation_state_base {};
 
 enum class operation_status { start, running, done };
 
+template <class... Senders> struct completion_signatures;
+
+template <class Sender, class... Senders>
+struct completion_signatures<Sender, Senders...> {
+  using AddSigl = stdexec::__minvoke<
+      stdexec::__mconcat<stdexec::__q<stdexec::completion_signatures>>,
+      stdexec::completion_signatures_of_t<Senders>...>;
+
+  using type =
+      stdexec::make_completion_signatures<Sender, stdexec::no_env, AddSigl>;
+};
+
+template <class... Senders>
+using completion_signatures_t =
+    typename completion_signatures<Senders...>::type;
+
+template <class Sig> struct signature_to_tuple;
+
+template <class Ret, class... Args> struct signature_to_tuple<Ret(Args...)> {
+  using type = std::tuple<Ret, Args...>;
+};
+
+template <class Sig>
+using signature_to_tuple_t = typename signature_to_tuple<Sig>::type;
+
+template <class... Senders>
+using result_type_t =
+    stdexec::__mapply<stdexec::__transform<stdexec::__q<signature_to_tuple_t>,
+                                           stdexec::__mbind_front_q<
+                                               std::variant, std::monostate>>,
+                      completion_signatures_t<Senders...>>;
+
+template <class... Senders> class receiver;
+
 template <class... Senders> struct shared_state {
   static constexpr std::size_t N = sizeof...(Senders);
+  using start_fn = void (*)(operation_state_base<Senders...>*);
   using complete_fn = void (*)(operation_state_base<Senders...>*);
-
-  std::tuple<Senders...> senders_;
-  std::atomic<operation_status> state_{operation_state::start};
-
-  stdexec::in_place_stop_source stop_source_;
-
-  std::atomic<std::size_t> count_{0};
-  std::array<result_type_t<Senders...>, N> results_{};
 
   std::mutex vtable_mutex_;
   std::array<start_fn, N> start_{};
+  std::array<complete_fn, N> complete_{};
   std::array<operation_state_base<Senders...>*, N> ops_{};
+
+  std::tuple<Senders...> senders_;
+  std::tuple<stdexec::connect_result_t<Senders, receiver<shared_state>>...>
+      operation_states_;
+  std::atomic<operation_status> state_{operation_status::start};
+
+  std::array<stdexec::in_place_stop_source, N> local_stop_sources_{};
+  stdexec::in_place_stop_source global_stop_source_;
+
+  std::array<result_type_t<Senders...>, N> results_{};
+  std::atomic<std::size_t> count_{0};
+  std::atomic<std::size_t> connections_{0};
 };
 
-template <class Receiver, class Op> class receiver {
-  Op* op_;
+template <class... Senders> class receiver {
+  shared_state<Senders...>* shared_state_;
 
   template <class CPO, class... Args> void notify(CPO, Args&&... args) {
-    op_->notify(CPO{}, (Args &&) args);
+    auto rank = shared_state_->count_.fetch_add(1, std::memory_order_relaxed);
+    assert(rank < sizeof...(Senders));
+    if (shared_state_->local_stop_sources_[rank].stop_requested()) {
+      state_.results_[rank]
+          .template emplace<std::tuple<stdexec::set_stopped_t>>(
+              stdexec::set_stopped);
+    } else {
+      try {
+        state_.results_[rank]
+            .template emplace<std::tuple<CPO, std::decay_t<Args>>>(
+                CPO{}, (Args &&) args...);
+      } catch (...) {
+        state_.results_[rank]
+            .template emplace<
+                std::tuple<stdexec::set_error_t, std::exception_ptr>>(
+                stdexec::set_error, std::current_exception());
+      }
+    }
+    if (shared_state_->complete_[rank]) {
+      auto count =
+          shared_state_->connections_.fetch_sub(1, std::memory_order_relaxed);
+      if (count == 1) {
+        shared_state_->global_stop_source_.request_stop();
+      }
+      shared_state_->complete_[rank](shared_state_->ops_[rank]);
+    }
   }
 
-  friend auto tag_invoke(stdexec::get_env_t, const receiver& self) {}
+  friend auto tag_invoke(stdexec::get_env_t, const receiver& self) noexcept
+      -> env_t {
+    using with_token = stdexec::__with<stdexec::get_stop_token_t,
+                                       stdexec::in_place_stop_token>;
+    return env_t{with_token{op_->global_stop_source_.get_token()}};
+  }
 };
 
-template <class Sender, class Receiver, class... Senders>
+template <class Receiver, class... Senders>
 class operation : public operation_state_base {
   using shared_state_ptr = std::shared_ptr<shared_state<Senders...>>;
 
@@ -42,42 +118,39 @@ class operation : public operation_state_base {
   int index_;
   Receiver receiver_;
   operation_status status_{operation_status::start};
-  stdexec::connect_result_t<Sender, receiver<Receiver, operation>>
-      operation_state_;
+
+  struct on_stop_requested {
+    stdexec::in_place_stop_source& stop_source_;
+    void operator()() noexcept { stop_source_.request_stop(); }
+  };
+  using on_stop = std::optional<typename stdexec::stop_token_of_t<
+      stdexec::env_of_t<Receiver>&>::template callback_type<on_stop_requested>>;
+  on_stop on_receiver_stop_{};
+  using on_shared_stop = std::optional<
+      stdexec::inplace_stop_token::callback_type<on_stop_requested>>;
+  on_shared_stop on_shared_stop_{};
 
   operation(shared_state_ptr&& state, int index, Receiver&& receiver)
       : state_{std::move(state)}, index_{index}, receiver_{(Receiver &&)
                                                                receiver} {
+    on_stop_.emplace(std::exec::get_stop_token(receiver_),
+                     on_stop_requested{state_->stop_source_});
+    on_shared_stop_.emplace(state_->stop_source_.get_token(),
+                            on_stop_requested{state_->stop_source_});
     std::scoped_lock lock{state_->vtable_mutex_};
     state_->count_.fetch_add(1, std::memory_order_relaxed);
     state_->complete_[index_] = [](operation_state_base<Senders...>* op) {
       static_cast<operation*>(op)->complete();
     };
-    state_->complete_[index_] = [](operation_state_base<Senders...>* op) {
+    state_->start_[index_] = [](operation_state_base<Senders...>* op) {
       static_cast<operation*>(op)->start();
     };
     state_->ops_[index_] = this;
   }
 
-  auto start() noexcept -> void { 
+  auto start() noexcept -> void {
     state_ = operation_state::running;
     operation_state_.start();
-  }
-
-  template <typename CPO, typename... Args>
-  auto notify(CPO, Args&&... args) noexcept -> void {
-    try {
-      state_.results_[index_]
-        .template emplace<std::tuple<CPO, std::decay_t<Args>>>(
-            CPO{}, (Args &&) args...);
-    } catch (...) {
-      state_.results_[index_].template emplace<std::exception_ptr>(
-          std::current_exception());
-    }
-    if (state_.count_.fetch_sub(1, std::memory_order_relaxed) == 1) {
-      state_.stop_source_.request_stop();
-    }
-    complete();
   }
 
   auto complete() noexcept -> void {
@@ -156,12 +229,33 @@ template <class... Senders> class sender {
     if (!(0 <= index_ && index_ < sizeof...(Senders))) {
       throw std::logic_error("invalid index");
     }
-    return stdexec::connect((shared_state_ptr &&) self.state_,
-                            std::exchange(index_, -1), (Receiver &&) receiver);
+
+    return operation<Receiver, Senders...>((shared_state_ptr &&) self.state_,
+                                           std::exchange(index_, -1),
+                                           (Receiver &&) receiver);
   }
 };
 
+struct for_each_t {
+  template <typename... Senders>
+  auto operator()(Senders&&... senders) const {
+    auto state = std::make_shared<shared_state<std::decay_t<Senders>...>>();
+    state->senders_ = std::tuple{((Senders&&) senders)...};
+    std::array<sender<std::decay_t<Senders>...>, sizeof...(Senders)> senders{};
+    for (std::size_t i = 0; i < sizeof...(Senders); ++i) {
+      senders_[i].state_ = state;
+      senders_[i].index_ = i;
+    }
+    return senders;
+  }
+};
+
+inline constexpr for_each_t for_each;
+
 } // namespace for_each_
+
+using for_each_::for_each;
+
 } // namespace gsenders
 
 #endif
