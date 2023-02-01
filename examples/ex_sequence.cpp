16adb547af96ee4286724766a95b97b5d2cbd09f
Nadolski, Maikel <maikel.nadolski@gmail.com>
2023-02-01 09:34:05 +0100

    Use at_coroutine_stop in example of sequence

--- examples/ex_sequence.cpp
+++ examples/ex_sequence.cpp
@@ -1,43 +1,51 @@
+#include "glib-senders/at_coroutine_exit.hpp"
 #include "glib-senders/file_descriptor.hpp"
-#include "glib-senders/sequence.hpp"
 #include "glib-senders/glib_io_context.hpp"
+#include "glib-senders/sequence.hpp"
 
 #include <array>
 #include <iostream>
 #include <ranges>
 
-int main() {
-  using namespace gsenders;
-  using namespace std::chrono_literals;
+using namespace gsenders;
+using namespace std::chrono_literals;
 
-  glib_io_context context{};
-  std::thread runner{[&] { context.run(); }};
-
-  file_descriptor fd1{STDIN_FILENO};
-  file_descriptor fd2{STDIN_FILENO};
-  char buffer1[128];
-  char buffer2[128];
-
-  {
-    enum class operation_kind { read, timeout };
-    // sequence returns a stream of senders
-    auto queue = sequence(async_read_some(fd1, buffer1), wait_for(10s));
-    for (stdexec::sender auto work : queue) {
-      auto result = stdexec::sync_wait(std::move(work) | stdexec::then([](auto... args) -> int {
-        if constexpr (sizeof...(args)) {
-          return 1;
-        }
-        return 0;
-      }));
-      if (result && std::get<0>(*result)) {
-        std::cout << "Read!\n";
-        break;
-      } else {
-        std::cout << "Timeout!\n";
-      }
+template <class... Senders>
+exec::task<void> play(sequence_::stream<Senders...>& seq) {
+  co_await at_coroutine_exit(
+      [](auto seq) -> exec::task<void> { co_await seq->cleanup(); }, &seq);
+  for (stdexec::sender auto work : seq) {
+    auto result =
+        co_await stdexec::then(std::move(work), [](auto... args) -> int {
+          if constexpr (sizeof...(args)) {
+            return 1;
+          }
+          return 0;
+        });
+    if (result) {
+      std::cout << "STDIN\n";
+    } else {
+      std::cout << "Timeout\n";
     }
+    break;
   }
+}
 
-  context.stop();
-  runner.join();
+exec::task<void> play(file_descriptor fd, std::span<char> buffer) {
+  std::cout << "Press enter or wait 2 seconds to exit\n";
+  sequence_::stream seq = sequence(async_read_some(fd, buffer), wait_for(2s));
+  co_await play(seq);
+}
+
+int main() {
+  glib_io_context context{};
+
+  file_descriptor fd{STDIN_FILENO};
+  char buffer[128];
+  stdexec::start_detached(
+      play(fd, buffer)                                              //
+      | stdexec::then([&context] { return context.stop(); }) //
+      | stdexec::upon_stopped([] { std::cout << "Stopped\n"; }));
+
+  context.run();
 }
