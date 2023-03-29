adf4a6d11c2487afe4ac4e64af126f204d74a355
Nadolski, Maikel <maikel.nadolski@gmail.com>
2023-03-29 14:59:19 +0200

    Fix enumerate

--- examples/ex_repeat_effect.cpp
+++ examples/ex_repeat_effect.cpp
@@ -1,8 +1,11 @@
 #include <glib-senders/repeat_effect.hpp>
 #include <glib-senders/sequence_join.hpp>
 #include <glib-senders/let_value_each.hpp>
+#include <glib-senders/enumerate.hpp>
 
 #include <exec/variant_sender.hpp>
+
+#include <iostream>
 
 using namespace gsenders;
 
@@ -11,8 +14,15 @@ using just_t = decltype(stdexec::just());
 
 int main() {
   stdexec::sync_wait(
-      repeat_effect(stdexec::just())
-      | let_value_each([] { return stdexec::just_stopped(); })
-      | sequence_join()
-  );
+    repeat_effect(stdexec::just()) //
+    | enumerate()                  //
+    | let_value_each([](int n) -> exec::variant_sender<just_stopped_t, just_t> {
+        std::cout << n << std::endl;
+        if (n < 10) {
+          return stdexec::just();
+        }
+        return stdexec::just_stopped();
+      }) //
+    | sequence_join()
+    | stdexec::then([] { std::cout << "done.\n"; }));
 }
--- source/glib-senders/enumerate.hpp
+++ source/glib-senders/enumerate.hpp
@@ -20,39 +20,51 @@ namespace gsenders {
       }
     };
 
+    template <class Int>
+    struct increase_count {
+      std::atomic<Int>* count_;
+
+      template <class... Args>
+      auto operator()(Args&&... args) const {
+        const Int count = count_->fetch_add(1, std::memory_order_relaxed);
+        return stdexec::just(count, static_cast<Args&&>(args)...);
+      }
+    };
+
     template <std::integral Int, class Receiver>
     class receiver {
      public:
-      explicit receiver(operation_base<Int, Receiver>& op) noexcept
+      explicit receiver(operation_base<Int, Receiver>* op) noexcept
         : op_(op) {
       }
 
      private:
-      template <class... Args>
-      friend void tag_invoke(stdexec::set_value_t, receiver&& self, Args&&... args) noexcept {
-        stdexec::set_value(
-          static_cast<Receiver&&>(self.op_.rcvr_), self.op_.count_, static_cast<Args&&>(args)...);
+      friend void tag_invoke(stdexec::set_value_t, receiver&& self) noexcept {
+        stdexec::set_value(static_cast<Receiver&&>(self.op_->rcvr_));
       }
 
       template <stdexec::sender Item>
       friend auto tag_invoke(set_next_t, receiver& self, Item&& item) noexcept {
-        return set_next(self.op_.rcvr_, stdexec::let_value(
-          static_cast<Item&&>(item), [&self]<class... Args>(Args&&... args) {
-            const Int count = self.op_.count_.fetch_add(1, std::memory_order_relaxed);
-            return stdexec::just(count, static_cast<Args&&>(args)...);
-          }));
+        return set_next(
+          self.op_->rcvr_,
+          stdexec::let_value(static_cast<Item&&>(item), increase_count<Int>{&self.op_->count_}));
       }
 
       template <class Error>
       friend void tag_invoke(stdexec::set_error_t, receiver&& self, Error&& error) noexcept {
-        stdexec::set_error((Receiver&&) self.op_.rcvr_, (Error&&) error);
+        stdexec::set_error((Receiver&&) self.op_->rcvr_, (Error&&) error);
       }
 
       friend void tag_invoke(stdexec::set_stopped_t, receiver&& self) noexcept {
-        stdexec::set_value((Receiver&&) self.op_.rcvr_, self.op_.count_);
+        stdexec::set_value((Receiver&&) self.op_->rcvr_);
       }
 
-      operation_base<Int, Receiver>& op_;
+      friend stdexec::env_of_t<Receiver>
+        tag_invoke(stdexec::get_env_t, const receiver& self) noexcept {
+        return stdexec::get_env(self.op_->rcvr_);
+      }
+
+      operation_base<Int, Receiver>* op_;
     };
 
     template <std::integral Int, class Sender, class Receiver>
@@ -62,7 +74,7 @@ namespace gsenders {
       template <stdexec::__decays_to<Sender> Sndr, stdexec::__decays_to<Receiver> Rcvr>
       operation(Sndr&& sndr, Rcvr&& rcvr, Int initial_val)
         : operation_base<Int, Receiver>(static_cast<Rcvr&&>(rcvr), initial_val)
-        , op_(stdexec::connect(sndr, receiver{*this})) {
+        , op_(stdexec::connect(sndr, receiver<Int, Receiver>{this})) {
       }
 
      private:
@@ -83,6 +95,13 @@ namespace gsenders {
         , initial_val_{initial_val} {
       }
 
+      using completion_signatures = stdexec::completion_signatures_of_t<Sender>;
+
+      template <class Item>
+      using next_item_t = decltype(stdexec::let_value(
+        stdexec::__declval<Item&&>(),
+        stdexec::__declval<increase_count<Int>>()));
+
       template <stdexec::receiver Rcvr>
       friend auto tag_invoke(stdexec::connect_t, sender&& self, Rcvr&& rcvr) {
         return operation<Int, Sender, std::decay_t<Rcvr>>{
@@ -91,10 +110,16 @@ namespace gsenders {
     };
 
     struct enumerate_t {
-      template <std::integral Int, class Sender>
+      template <class Sender, std::integral Int = int>
         requires stdexec::sender<Sender>
       auto operator()(Sender&& sndr, Int initial_val = Int{}) const noexcept {
         return sender<Int, std::decay_t<Sender>>{static_cast<Sender&&>(sndr), initial_val};
+      }
+
+      template <std::integral Int = int>
+      auto operator()(Int initial_val = Int{}) const noexcept
+        -> stdexec::__binder_back<enumerate_t, Int> {
+        return {{}, {}, {initial_val}};
       }
     };
   } // namespace enumerate_
